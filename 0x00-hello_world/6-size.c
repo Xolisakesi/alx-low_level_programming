33902e86dcb76e7e93b80e24b482c3743cf24920
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-10 20:15:05 +0000

    6-size.c hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(_char));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(_int));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(_lint));
 printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(Lint));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(_float));
+printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(_float));
 return (0);
 }
