902a5dda76cf8694101d676f2208c50d1f27d3a3
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-10 21:32:51 +0000

    101-quote.c script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 1 (sucess)
+ */
+int main(void)
+{
+write(STDOUT_FILENO, "and that piece of art is usefl
+\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
