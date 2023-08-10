1d41f115454e8ec87eede2c60e27a6bb9c7982e1
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-10 20:04:31 +0000

    6-size.c script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+char _char;
+int _int;
+long int _lint;
+long long int Lint;
+float _float;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(_char));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(_int));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(_lint));
+printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(Lint));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(_float));
+return (0);
+}
