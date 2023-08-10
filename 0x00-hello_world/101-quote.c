3cfc8078b7ff5662935e804a6fadb774870b8010
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-10 23:26:56 +0000

    101-quote.c script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always return 1 (sucess)
- */
+*/
 int main(void)
 {
-write(STDERR_FILENO,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
