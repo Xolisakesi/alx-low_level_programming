8efb483c9cee5c5ab5c09c7504e49f78b3c45da5
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-11 22:38:31 +0000

    100-print_comb3.c betty hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ for (a = '0'; a <= '8'; a++)
 {
 for (b = '0'; b <= '9'; b++)
 {
-if ((a != b)  && ( a < b))
+if ((a != b) && (a < b))
 {
 putchar(a);
 putchar(b);
 }
 if (((a != '8') || (a == '8' && b != '9')) &&
-((a < b) &&(a != b)))
+((a < b) && (a != b)))
 {
 putchar(',');
 putchar(' ');
