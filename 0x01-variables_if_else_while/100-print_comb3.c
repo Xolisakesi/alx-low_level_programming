4e30987af2a22b462e65aaf42f646e50dea8b762
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-11 20:44:34 +0000

    100-print_comb3.c script

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers 00 to 99
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+int a, b;
+for (a = '0'; a <= '8'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+putchar(a);
+putchar(b);
+if ((a != '8') || (a == '8' && b != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
