9553b232796d39e16947571f3dc54950c607a8f7
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-16 00:32:05 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		print_alphabet();
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+
 	}
 	_putchar('\n');
 }
