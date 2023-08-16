854a1c5cda715f797cfdd0e160b8f772a4b62263
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-16 18:13:30 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,26 +9,22 @@ void times_table(void)
 {
 	int i, j, r;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			r = i * j;
-		if (r < 10)
-		{
-			_putchar(' ');
-			_putchar(r + '0');
-		}
-		else
-		{
-			_putchar((r / 10) + '0');
-			_putchar((r % 10) + '0');
-		}
-		if (j != 9)
+		_putchar('0');
+
+		for (j = 1; j < 10; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
+			r = j * i;
+		if (r < 10)
+		{
+			_putchar(' ');
 		}
+		else
+			_putchar((r / 10) + '0');
+			_putchar((r % 10) + '0');
 		}
 		_putchar('\n');
 	}
