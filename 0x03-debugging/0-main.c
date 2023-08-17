2019ff9c677bb9cb0621df9817ffbcbfbef8b804
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-17 04:44:58 +0000

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - test a fuction that prints if an integer
+ * is positive or negetive, test for 0 value
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int test;
+
+	test = 0;
+	positive_or_negative(test);
+	return (0);
+}
