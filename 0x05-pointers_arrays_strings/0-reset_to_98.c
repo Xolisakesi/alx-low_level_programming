c02132a011ab2299d8317abe6dc2da13f8279d6d
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-21 10:37:46 +0000

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - function takes a pointer to an int
  * as a parameter and updates the value it points to
- * @*n: poinrer to an int parameter
+ * @n: pointer to an int parameter
  * Return: void nothing
  */
 void reset_to_98(int *n)
