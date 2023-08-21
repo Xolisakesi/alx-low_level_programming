d303a5922da16dba513998863aa0c88bbe431503
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-21 13:29:45 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - fumction prints a string in reverse
+ * @s: Pointer to string
+ * Return: void nothing
+ */
+void print_rev(char *s)
+{
+int len = 0;
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+len++;
+}
+for (j = len; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
+}
