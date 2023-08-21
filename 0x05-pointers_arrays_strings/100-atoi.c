c592517b3121692bd18645e2be28cc6f6fa7bd8f
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-21 22:21:25 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,19 +6,25 @@
  */
 int _atoi(char *s)
 {
-int r = 0;
+unsigned int r = 0;
 int sign = 1;
 int i = 0;
 
-if (s[0] == '-')
+while (s[i] != '\0')
 {
-sign = - 1;
-i = 1;
+if (s[i] == '-')
+{
+sign = -sign;
 }
-while (s[i] >= '0' && s[i] <= '9')
+else if (s[i] >= '0' && s[i] <= '9')
 {
 r = r * 10 + (s[i] - '0');
+}
+else if (r != 0)
+{
+break;
+}
 i++;
 }
-return (r * sign);
+return (r *sign);
 }
