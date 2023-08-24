1a3d31d63f814983944633629243744ab7c9a67d
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-24 22:17:16 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,33 +7,30 @@
  */
 char *cap_string(char *s)
 {
-static int i, insideWord;
+static int i;
 
-for (i = 0; s[i] != '\0'; i++)
+while (s[i])
 {
-if ((s[i] >= 'a' && s[i] <= 'z') ||
-	(s[i] >= 'A' && s[i] <= 'Z'))
+while (!(s[i] >= 'a' && s[i] <= 'z'))
 {
-if (!insideWord)
-{
-insideWord = 1;
-if (s[i] >= 'a' && s[i] <= 'z')
-{
+i++;
+}
+if (s[i - 1] == ' ' ||
+s[i - 1] == '\t' ||
+s[i - 1] == '\n' ||
+s[i - 1] == ',' ||
+s[i - 1] == ';' ||
+s[i - 1] == '.' ||
+s[i - 1] == '!' ||
+s[i - 1] == '?' ||
+s[i - 1] == '"' ||
+s[i - 1] == '(' ||
+s[i - 1] == ')' ||
+s[i - 1] == '{' ||
+s[i - 1] == '}' ||
+i == 0)
 s[i] -= 32;
-}
-}
-else
-{
-if (s[i] >= 'A' && s[i] <= 'Z')
-{
-s[i] += 32;
-}
-}
-}
-else
-{
-insideWord = 0;
-}
+i++;
 }
 return (s);
 }
