0a37e550ea1a1214c6849629e410d68de520fdcd
Xolisa Kesi <xolisakesi@gmail.com>
2023-08-25 00:55:59 +0000

    advanced tasks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - a function that prints an integer.
+ * @n: an interger argument passed to the function
+ * Return: returns void nothing
+ */
+void print_number(int n)
+{
+unsigned int _number;
+_number = n;
+if (n < 0)
+{
+_putchar('-');
+_number = -n;
+}
+if (_number / 10 != 0)
+{
+print_number(_number / 10);
+}
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_buffer - function that prints a buffer
+ * @b: pointer to data
+ * @size: size of buffer
+ * Return: void nothing
+ */
+void print_buffer(char *b, int size)
+{
+static int i, j, k;
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+while (k < size)
+{
+
+}
+}
