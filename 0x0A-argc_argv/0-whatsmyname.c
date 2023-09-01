c118e585b56a8ed91ae05a40def603fb2b9e2e60
Xolisa Kesi <xolisakesi@gmail.com>
2023-09-01 10:54:37 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prifnts program name
+ * @argc: numner of arguments counter
+ * @argv: argument verctor
+ * Return: Always 0 sucess
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
