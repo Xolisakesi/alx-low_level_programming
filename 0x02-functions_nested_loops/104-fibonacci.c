9b8181d543f4dc3a1b1945dd117dbafaec4ad291
Xolisa Kesi <xolisakesi@gmail.com>
2023-09-10 21:03:32 +0000

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - finds and prints first 98 fibonichi numbers
+ * Return: always 0 sucess
+ */
+
+int main(void)
+{
+unsigned long j = 1, k = 2;
+int i = 0;
+
+printf("%lu", j);
+for (; i < 99; ++i)
+{
+printf(", %lu", k);
+k += j;
+j = k - j;
+}
+printf("\n");
+return (0);
+}
