68b5d615e96ef1ef9d229775332682d35624ad36
Xolisa Kesi <xolisakesi@gmail.com>
2023-09-10 19:39:03 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,24 +2,50 @@
 #include "main.h"
 /**
  * print_times_table - prints nxtable
- *
  * @n: parameter passed to the function
- *
  * Return: Always 0 (sucess)
- *
  **/
+
 void print_times_table(int n)
 {
-	if (n < 0 || n > 0)
-	{
-		return (0);
-	}
-	for (int i = 0; i <= n; i++)
-	{
-		for (int j = 0; j <= n; j++)
-		{
-			int result = i*j;
+int i, j, k;
 
-		}
-	}
+if (n < 0 || n > 15)
+{
+return;
+}
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+k = i * j;
+if (j == 0)
+_putchar('0' + k);
+else if (k < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10 && k <= 99)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
