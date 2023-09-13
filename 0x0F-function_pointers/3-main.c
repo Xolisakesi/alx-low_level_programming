1350016402fbc7928d1d9ab1c54ba7d2d24463cc
Xolisa Kesi <xolisakesi@gmail.com>
2023-09-13 21:24:49 +0000

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+
 /**
- * main - fuctiom peforms simple arithmetic operations
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
+ * main - program that performs simple arithmetic operations
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-int op, arg1, arg2, result;
-op = atoi(argv[2]);
+int arg1, arg2, result;
+int (*func)(int, int);
+
 if (argc != 4)
 {
 puts("Error");
-return (98);
-}
-if (op != 1 || (op != '+' && op != '-' && op != '*' && op != '/' && op != '%'))
-{
-puts("Error");
-return (99);
+exit(98);
 }
 arg1 = atoi(argv[1]);
 arg2 = atoi(argv[3]);
-result = get_op_func(argv[2])(arg1, arg2);
-
-if (!result)
+func = get_op_func(argv[2]);
+if (!func)
 {
 puts("Error");
-return (99);
+exit(99);
 }
-else if ((*argv[2] == '/' || *argv[2] == '%') && arg2 == 0)
+if ((*argv[2] == '/' || *argv[2] == '%') && arg2 == 0)
 {
 puts("Error");
-return (100);
+exit(100);
 }
+result = func(arg1, arg2);
 printf("%d\n", result);
 return (0);
 }
+
