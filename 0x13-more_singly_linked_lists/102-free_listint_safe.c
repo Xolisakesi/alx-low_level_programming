b34da7bd66f720248fb245e4e5a246757ea1fc71
Xolisakesi <xolisakesi@gmail.com>
2023-09-26 20:40:05 +0200

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * free_listint_safe - Safely frees a linked list and
+ * its memory.
+ * @h: Pointer to a pointer to the head of the
+ * linked list.
+ * Description: Frees all nodes of the linked list and
+ * its memory,
+ * safely handling the case where *h is NULL.
+ * Return: The number of nodes freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+size_t count = 0;
+listint_t *temp;
+
+if (!h || !*h)
+return (count);
+
+while (*h)
+{
+if (*h < (*h)->next)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+count++;
+}
+else
+{
+free(*h);
+*h = NULL;
+count++;
+break;
+}
+}
+return (count);
+}
