c6d7de77f8daac8df301c65473e83fa14d2c0ff9
Xolisakesi <xolisakesi@gmail.com>
2023-12-22 00:43:41 +0200

    hash tables

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,57 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_set - Add or update an element in a hash table.
+* @ht: A pointer to the hash table.
+* @key: The key to add - cannot be an empty string.
+* @value: The value associated with key.
+*
+* Return: Upon failure - 0. Otherwise - 1.
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+hash_node_t *new, *current;
+char *value_copy;
+unsigned long int index;
+
+if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+return (0);
+
+value_copy = strdup(value);
+if (value_copy == NULL)
+return (0);
+
+index = key_index((const unsigned char *)key, ht->size);
+
+for (current = ht->array[index]; current; current = current->next)
+{
+if (strcmp(current->key, key) == 0)
+{
+free(current->value);
+current->value = value_copy;
+return (1);
+}
+}
+
+new = malloc(sizeof(hash_node_t));
+if (new == NULL)
+{
+free(value_copy);
+return (0);
+}
+
+new->key = strdup(key);
+if (new->key == NULL)
+{
+free(new);
+free(value_copy);
+return (0);
+}
+
+new->value = value_copy;
+new->next = ht->array[index];
+ht->array[index] = new;
+
+return (1);
+}
+
