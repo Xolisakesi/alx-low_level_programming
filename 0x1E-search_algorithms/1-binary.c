c8f43f1204da2daeec6769e5f81843cf667efa34
Xolisakesi <xolisakesi@gmail.com>
2024-04-10 11:57:40 +0200

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,43 +1,61 @@
+#include "search_algos.h"
 #include <stdio.h>
+/**
+* recursive_search - searches for a value in an array of
+* integers using the Binary search algorithm
+*
+*
+* @array: input array
+* @size: size of the array
+* @value: value to search in
+* Return: index of the number
+*/
+int recursive_search(int *array, size_t size, int value)
+{
+size_t half = size / 2;
+size_t i;
+
+if (array == NULL || size == 0)
+return (-1);
+
+printf("Searching in array");
+
+for (i = 0; i < size; i++)
+printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+printf("\n");
+
+if (half && size % 2 == 0)
+half--;
+
+if (value == array[half])
+return ((int)half);
+
+if (value < array[half])
+return (recursive_search(array, half, value));
+
+half++;
+
+return (recursive_search(array + half, size - half, value) + half);
+}
 
 /**
-* binary_search - searches for a value in a sorted array of integers
-* using the Binary search algorithm
-* @array: pointer to the first element of the array to search in
-* @size: number of elements in the array
-* @value: value to search for
+* binary_search - calls to binary_search to return
+* the index of the number
 *
-* Return: index where value is located, or -1 if not found or array is NULL
+* @array: input array
+* @size: size of the array
+* @value: value to search in
+* Return: index of the number
 */
 int binary_search(int *array, size_t size, int value)
 {
-size_t i, mid;
-size_t left = 0;
-size_t right = size - 1;
+int index;
 
-if (array == NULL)
+index = recursive_search(array, size, value);
+
+if (index >= 0 && array[index] != value)
 return (-1);
 
-while (left <= right)
-{
-mid = (left + right) / 2;
-
-printf("Searching in array: ");
-for (i = left; i <= right; i++)
-{
-printf("%d", array[i]);
-if (i < right)
-printf(", ");
-}
-printf("\n");
-
-if (array[mid] == value)
-return (mid);
-
-if (array[mid] < value)
-left = mid + 1;
-else
-right = mid - 1;
-}
-return (-1); /* Value not found*/
+return (index);
 }
